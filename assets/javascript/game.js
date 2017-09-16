d061434ded266e3c374ac828e3fe4fbf684e512d
BakerShoeMaker <info@quackenworth.com>
2017-09-15 20:22:37 -0700

    intial work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,27 @@
 console.log("Hello World");
+
+function loadPage(){
+
+
+    //$("#correct").html("Correct");
+    //$("#wrong").html("Wrong: ");
+    //$("#time").html("2:00");
+
+    //$("#quizSection").html("Let's get started");
+    $("#buttonID").click(function () {
+        $(this).remove();
+        startGame();
+    });
+
+}
+
+function startGame(){
+alert("hello!");
+
+
+}
+
+//when page is loaded.
+$(document).ready(function () {
+    loadPage();
+}
